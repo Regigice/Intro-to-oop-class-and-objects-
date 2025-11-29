a4981014c7266d5ea4ea6dbf6b623a01e2436a4f
Regigice <darnellandersonplayz@gmail.com>
2025-11-29 15:15:47 -0400

    Add Main class to demonstrate Car object usage

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+//object = An entity that holds data (attributes)
+ //         and can perform actions(methods)
+ //
+
+    public static void main(String[] args){
+
+
+        Car car = new Car();
+
+        car.isRunning = true;
+
+        System.out.println(car.make);
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.isRunning);
+                 
+    }
+
+
+}
